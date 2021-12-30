1be52031f4c2da06bd3eaf9e915624e81b16e60b
Micael Lima <micaelcomp@alu.ufc.br>
2021-12-30 10:05:53 -0300

    feat: ensure RegisterUserController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/register-user.ts
+++ src/presentation/controllers/register-user.ts
@@ -3,7 +3,13 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 
 class RegisterUserController {
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const fields = ['name', 'nickname', 'email', 'password'];
+    const fields = [
+      'name',
+      'nickname',
+      'email',
+      'password',
+      'passwordConfirmation',
+    ];
 
     // eslint-disable-next-line no-restricted-syntax
     for (const field of fields) {
