fced90cb80bc064faf5981d90d24db31243caa7b
Micael Lima <micaelcomp@alu.ufc.br>
2021-12-30 09:33:36 -0300

    refactor: create http helper

--- src/presentation/controllers/register-user.ts
+++ src/presentation/controllers/register-user.ts
@@ -1,26 +1,18 @@
+import { badRequest } from 'presentation/helpers/http-codes';
 import { HttpRequest, HttpResponse } from 'presentation/protocols/http';
 
 class RegisterUserController {
   async handle(request: HttpRequest): Promise<HttpResponse> {
     if (!request.body.name) {
-      return {
-        statusCode: 400,
-        data: new Error('Missing param: name'),
-      };
+      return badRequest(new Error('Missing param: name'));
     }
 
     if (!request.body.nickname) {
-      return {
-        statusCode: 400,
-        data: new Error('Missing param: nickname'),
-      };
+      return badRequest(new Error('Missing param: nickname'));
     }
 
     if (!request.body.email) {
-      return {
-        statusCode: 400,
-        data: new Error('Missing param: email'),
-      };
+      return badRequest(new Error('Missing param: email'));
     }
 
     return undefined;
