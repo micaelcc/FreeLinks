5b08f6b66510f164113116005c694bebc7e709f4
Micael Lima <micaelcomp@alu.ufc.br>
2021-12-30 10:21:04 -0300

    feat: ensure RegisterUserController returns 400 if password confirmation fails

--- src/presentation/controllers/register-user.ts
+++ src/presentation/controllers/register-user.ts
@@ -11,11 +11,17 @@ class RegisterUserController {
       'passwordConfirmation',
     ];
 
+    const { password, passwordConfirmation } = request.body;
+
     // eslint-disable-next-line no-restricted-syntax
     for (const field of fields) {
       if (!request.body[field]) {
         return badRequest(new Error(`Missing param: ${field}`));
       }
+    }
+
+    if (password !== passwordConfirmation) {
+      return badRequest(new Error('Invalid param: passwordConfirmation'));
     }
 
     return undefined;
