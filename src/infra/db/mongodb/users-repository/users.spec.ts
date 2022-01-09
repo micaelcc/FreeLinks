85797f4baaa26573f123c4664b04f756a1e88f27
Micael Lima <micaelcomp@alu.ufc.br>
2022-01-09 18:34:27 -0300

    test: ensure UsersMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/users-repository/users.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+import { UsersMongoRepository } from './users';
+
+describe('Users Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should return an account on success', async () => {
+    const sut = new UsersMongoRepository();
+
+    const user = await sut.save({
+      name: 'any_name',
+      nickname: 'any_nickname',
+      email: 'any_email',
+      password: 'any_password',
+    });
+
+    expect(user).toBeTruthy();
+    expect(user.id).toBeTruthy();
+    expect(user.name).toEqual('any_name');
+    expect(user.nickname).toEqual('any_nickname');
+    expect(user.email).toEqual('any_email');
+    expect(user.password).toEqual('any_password');
+  });
+});
