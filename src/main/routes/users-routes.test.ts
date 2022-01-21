3dfa924ea7818a4927365ee770c4bd5a236a76e9
Micael Lima <micaelcomp@alu.ufc.br>
2022-01-20 21:05:10 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/users-routes.test.ts
+++ src/main/routes/users-routes.test.ts
@@ -1,8 +1,23 @@
 import request from 'supertest';
 
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import { app } from '../config/app';
 
 describe('Users Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const usersCollection = MongoHelper.getCollection('users');
+
+    await usersCollection.deleteMany({});
+  });
+
   test('Should return an user on success', async () => {
     await request(app)
       .post('/api/signup')
