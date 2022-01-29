904a4ba50632a3f791c44b1cdc9499312d34de1f
Micael Lima <micaelcomp@alu.ufc.br>
2022-01-29 19:35:43 -0300

    feat: register user route

--- src/main/routes/users-routes.ts
+++ src/main/routes/users-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
+import { adaptRoute } from '../adapters/express-route-adapter';
+import { makeRegisterUserController } from '../factories/register-user';
+
 export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: 'ok' });
-  });
+  router.post('/signup', adaptRoute(makeRegisterUserController()));
 };
